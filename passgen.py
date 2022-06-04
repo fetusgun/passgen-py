c8e4bb8fe99b2439338020c628e7711713c6aa53
bananlabs <paulerzst@gmail.com>
2022-06-04 19:54:05 -0300

    program logic added

--- passgen.py
+++ passgen.py
@@ -1,16 +1,40 @@
 # -----------------------------------------
-# A simple password generator in python
+# A simple password generator in python for
+# learning purposes
 # Author: Paulo Ferr
 # Date: 04/06/2022
 # -----------------------------------------
 
 import random
 
+# Lists of Letters, Symbols and numbers to choose
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+print("****** | Password Generator | ******")
+number_of_letters = int(input("Choose how many letters goes in your password: \n")) 
+number_of_symbols = int(input(f"How many symbols?\n"))
+number_of_numbers = int(input(f"How many numbers?\n"))
+
+password_list = []
+
+# pick the letters from the list
+for character in range(1, number_of_letters + 1):
+    password_list.append(random.choice(letters))
+
+# pick the symbols from the list
+for character in range(1, number_of_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+# pick the numbers from the list
+for character in range(1, number_of_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = ""
+for character in password_list:
+    password += character
+
+print(f"Your generated password is > {password}")
